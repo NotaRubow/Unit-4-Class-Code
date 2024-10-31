1510f01db03081d70ab654fa29742ba6ff9de4b9
rubown_wlhs <rubown@wlhs.wlwv.k12.or.us>
2024-10-31 14:43:40 -0700

    Testing While statments

--- src/lesson1/Main.java
+++ src/lesson1/Main.java
@@ -1,9 +1,17 @@
 package lesson1;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         guessMyNumber();
-
+        Scanner input = new Scanner(System.in);
+        int secretNumber = (int)(Math.random()*10+1);
+        System.out.println("Guess my number between 1 and 10");
+        int numberguessed = input.nextInt();
+        while (numberguessed != secretNumber){
+            System.out.println("Try Again!");
+            numberguessed = input.nextInt();
+        }
+        System.out.println("Good Job!");
     }
 
     public static void guessMyNumber(){

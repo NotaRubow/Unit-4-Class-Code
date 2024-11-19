5648e86b0226a362ad7cc88ea95728851387a7f2
NotaRubow <rubown@wlhs.wlwv.k12.or.us>
2024-11-19 10:49:22 -0800

    Add files via upload

--- src/lesson1/Main.java
+++ src/lesson1/Main.java
@@ -1,20 +1,41 @@
 package lesson1;
+
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        guessMyNumber();
-        Scanner input = new Scanner(System.in);
-        int secretNumber = (int)(Math.random()*10+1);
-        System.out.println("Guess my number between 1 and 10");
-        int numberguessed = input.nextInt();
-        while (numberguessed != secretNumber){
-            System.out.println("Try Again!");
-            numberguessed = input.nextInt();
+        //guessMyNumber();
+        Scanner scan = new Scanner(System.in);
+
+        int startValue = 0;
+        while (startValue < 10) {
+            System.out.println(startValue);
+            startValue++;
         }
-        System.out.println("Good Job!");
+
+
+        while (true) {
+            System.out.print("Enter a number: ");
+            int userNum = scan.nextInt();
+            if (userNum == 8) {
+                break;
+            }
+        }
     }
 
-    public static void guessMyNumber(){
-
+    public static void guessMyNumber() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("I'm thinking of a number between 1 and 10.");
+        System.out.println("See if you can guess the number!");
+        int secretNumber = (int) (Math.random() * 10 + 1);
+        // for debugging System.out.println("Test: "+secretNumber);
+        System.out.print("Enter your guess: ");
+        int userGuess = input.nextInt();
+        while (secretNumber != userGuess) {
+            System.out.println("Try again!");
+            System.out.print("Enter your guess: ");
+            userGuess = input.nextInt();
+        }
+        System.out.println("Correct!");
     }
 }
